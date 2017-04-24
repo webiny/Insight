2dde50e01e5c5166bcade04f95e5598ca11c6405
Pavel910 <pavel@webiny.com>
2017-04-24 11:50:38 +0200

    Refactored for lazy loading

--- Js/Backend/Modules/ScoreCard/Views/Details.jsx
+++ Js/Backend/Modules/ScoreCard/Views/Details.jsx
@@ -15,44 +15,50 @@ Details.defaultProps = {
             <Webiny.Ui.LazyLoad modules={['Data', 'View', 'Link', 'List']}>
                 {(Ui) => (
                     <Ui.Data ui="stats" {...listProps}>
-                        {data => (
-                            <Ui.View.List>
-                                <Ui.View.Header
-                                    title={'Score Card/' + data[0].user.firstName + ' ' + data[0].user.lastName}
-                                    description="User score card details">
-                                    <Ui.Link type="default" align="right" route="Sauron.ScoreCard.List">Back</Ui.Link>
-                                </Ui.View.Header>
-                                <Ui.View.Body>
-                                    <h2>Level: {data[0].user.sauron.level} / Total score: {data[0].user.sauron.score}</h2>
-                                    <Ui.List.Table data={data}>
-                                        <Ui.List.Table.Row>
-                                            <Ui.List.Table.Field name="score" label="Score" sort="score" align="center">
-                                                {data => (
-                                                    <h1>{data.score}</h1>
-                                                )}
-                                            </Ui.List.Table.Field>
-                                            <Ui.List.Table.Field name="activities" align="left" label="Activities" sort="activities">
-                                                {data => (
-                                                    <div>
-                                                        <strong>Activities</strong>: {data.activities}<br/>
-                                                        <strong>Last activity on</strong>: <Ui.Filters.DateTime value={data.lastActivity}/>
-                                                    </div>
+                        {data => {
+                            if(!data.length) {
+                                return <Ui.List.Table.Empty/>
+                            }
 
-                                                )}
-                                            </Ui.List.Table.Field>
-                                            <Ui.List.Table.Field name="rule.name" align="left" label="Rule" sort="rule.name">
-                                                {data => (
-                                                    <span>
+                            return (
+                                <Ui.View.List>
+                                    <Ui.View.Header
+                                        title={'Score Card/' + data[0].user.firstName + ' ' + data[0].user.lastName}
+                                        description="User score card details">
+                                        <Ui.Link type="default" align="right" route="Sauron.ScoreCard.List">Back</Ui.Link>
+                                    </Ui.View.Header>
+                                    <Ui.View.Body>
+                                        <h2>Level: {data[0].user.sauron.level} / Total score: {data[0].user.sauron.score}</h2>
+                                        <Ui.List.Table data={data}>
+                                            <Ui.List.Table.Row>
+                                                <Ui.List.Table.Field name="score" label="Score" sort="score" align="center">
+                                                    {data => (
+                                                        <h1>{data.score}</h1>
+                                                    )}
+                                                </Ui.List.Table.Field>
+                                                <Ui.List.Table.Field name="activities" align="left" label="Activities" sort="activities">
+                                                    {data => (
+                                                        <div>
+                                                            <strong>Activities</strong>: {data.activities}<br/>
+                                                            <strong>Last activity on</strong>: <Ui.Filters.DateTime value={data.lastActivity}/>
+                                                        </div>
+
+                                                    )}
+                                                </Ui.List.Table.Field>
+                                                <Ui.List.Table.Field name="rule.name" align="left" label="Rule" sort="rule.name">
+                                                    {data => (
+                                                        <span>
                                                     <strong>{data.rule.name}</strong><br/>{data.rule.description}
                                                 </span>
-                                                )}
-                                            </Ui.List.Table.Field>
+                                                    )}
+                                                </Ui.List.Table.Field>
 
-                                        </Ui.List.Table.Row>
-                                    </Ui.List.Table>
-                                </Ui.View.Body>
-                            </Ui.View.List>
-                        )}
+                                            </Ui.List.Table.Row>
+                                        </Ui.List.Table>
+                                    </Ui.View.Body>
+                                </Ui.View.List>
+                            );
+                        }}
                     </Ui.Data>
                 )}
             </Webiny.Ui.LazyLoad>
