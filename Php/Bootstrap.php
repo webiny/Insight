f5d71ab89e676569b444f468fc420933b13857fb
Pavel910 <pavel@webiny.com>
2017-05-19 16:39:07 +0200

    Updated to reflect new life cycle "events" and package.json dependencies.

--- Php/Bootstrap.php
+++ Php/Bootstrap.php
@@ -1,15 +1,16 @@
 <?php
+
 namespace Apps\Sauron\Php;
 
-use Apps\Core\Php\DevTools\AbstractBootstrap;
 use Apps\Core\Php\Entities\User;
 use Apps\Core\Php\PackageManager\App;
 
-class Bootstrap extends AbstractBootstrap
+class Bootstrap extends \Apps\Core\Php\DevTools\LifeCycle\Bootstrap
 {
     public function run(App $app)
     {
-        User::onExtend(function(User $user){
+        parent::run($app);
+        User::onExtend(function (User $user) {
             // add the additional attributes
             // contains `score` and `level`
             $user->attr('sauron')->object();
